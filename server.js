873097e5b75ecb46256439ee4d2eac02c6a9baae
David Wilson <davidwil@posteo.de>
2021-01-01 20:10:11 +0000

    Split schema into own file

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const { buildSchema } = require('graphql');
 const sqlite3 = require('sqlite3').verbose();
 const cors = require('cors');
+
+const { schema } = require('./schema.js');
 
 let db = new sqlite3.Database('chinook.db', (err) => {
   if (err) {
@@ -12,155 +13,6 @@ let db = new sqlite3.Database('chinook.db', (err) => {
   console.log('Opened database');
 });
 
-// Schema
-const schema = buildSchema(`
-  type Query {
-    getAlbums: [Album],
-    getAlbum(albumId: Int!): Album,
-    getAlbumsByArtist(artistId: Int!): [Album],
-
-    getArtists: [Artist],
-    getArtist(artistId: Int!): Artist,
-
-    getCustomers: [Customer],
-    getCustomer(customerId: Int!): Customer,
-
-    getEmployees: [Employee],
-    getEmployee(employeeId: Int!): Employee,
-
-    getGenres: [Genre],
-    getGenre(genreId: Int!): Genre,
-
-    getInvoice(invoiceId: Int!): Invoice,
-    getInvoiceByCustomer(customerId: Int!): [Invoice]
-    getInvoiceLines(invoiceId: Int!): [InvoiceLine]
-
-    getMediaTypes: [MediaType]
-    getMediaType(mediaTypeId: Int!): MediaType
-
-    getPlaylists: [Playlist]
-    getPlaylist(playListId: Int!): Playlist
-
-    getPlaylistTracks(playListId: Int!): [PlaylistTrack]
-    getTrackPlaylists(trackId: Int!): [PlaylistTrack]
-
-    getTracks: [Track]
-    getTrack(trackId: Int!): Track
-    getTracksByAlbum(albumId: Int!): [Track]
-    getTracksByMediaType(mediaTypeId: Int!): [Track]
-    getTracksByGenre(genreId: Int!): [Track]
-  },
-  type Mutation {
-    setGenre(genreId: Int, genreName: String!): Genre
-    addGenre(genreName: String!): Genre
-    deleteGenre(genreId: Int!) : Int
-
-    setArtist(artistId: Int, artistName: String!): Artist
-    addArtist(artistName: String!): Artist
-    deleteArtist(artistId: Int!): Int
-
-    setAlbum(albumId: Int, albumName: String!, albumArtist: Int!) : Album
-    addAlbum(albumName: String!, albumArtist: Int!) : Album
-    deleteAlbum(albumId: Int!) : Int
-  }
-  type Album {
-    AlbumId: Int
-    Title: String
-    ArtistId: Int
-    ArtistName: String
-  },
-  type Artist {
-    ArtistId: Int
-    Name: String
-  },
-  type Customer {
-    CustomerId: Int
-    FirstName: String
-    LastName: String
-    Company: String
-    Address: String
-    City: String
-    State: String
-    Country: String
-    PostalCode: String
-    Phone: String
-    Fax: String
-    Email: String
-    SupportRepId: String
-    SupportRepFirstName: String
-    SupportRepLastName: String
-  },
-  type Genre {
-    GenreId: Int
-    Name: String
-  }
-  type Employee {
-    EmployeeId: Int
-    LastName: String
-    FirstName: String
-    Title: String
-    ReportsTo: Int
-    BirthDate: String
-    HireDate: String
-    Address: String
-    City: String
-    State: String
-    Country: String
-    PostalCode: String
-    Phone: String
-    Fax: String
-    Email: String
-    ReportsToFirstName: String
-    ReportsToLastName: String
-  },
-  type Invoice {
-    InvoiceId: Int
-    CustomerId: Int
-    InvoiceDate: String
-    BillingAddress: String
-    BillingCity: String
-    BillingState: String
-    BillingCountry: String
-    BillingPostalCode: String
-    Total: Float
-  }
-  type InvoiceLine {
-    InvoiceLineId: Int
-    InvoiceId: Int
-    TrackId: Int
-    UnitPrice: Float
-    Quantity: Int
-    TrackName: String
-  }
-  type MediaType {
-    MediaTypeId: Int
-    Name: String
-  }
-  type Playlist {
-    PlaylistId: Int
-    Name: String
-  }
-  type PlaylistTrack {
-    PlaylistId: Int
-    Track: Int
-  }
-  type Track {
-    TrackId: Int
-    Name: String
-    AlbumId: Int
-    MediaTypeId: Int
-    GenreId: Int
-    Composer: String
-    Milliseconds: Int
-    Bytes: Int
-    UnitPrice: Float
-    AlbumTitle: String
-    MediaTypeName: String
-    GenreName: String
-    ArtistId: Int
-    ArtistName: String
-  }
-`);
 
 const retrieveAlbums = (args) => {
   const sql = 'SELECT * FROM Album ORDER BY Title';
